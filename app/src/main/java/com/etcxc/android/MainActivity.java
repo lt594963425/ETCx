dca57ca0a4388e9ab279dbe89612005340184155
xwpeng <cocpl0101001@gmail.com>
2017-05-25 16:13:00 +0800

    修改包名为com.etcxc,android

--- app/src/main/java/com/etcxc/android/MainActivity.java
+++ app/src/main/java/com/etcxc/android/MainActivity.java
@@ -1,7 +1,9 @@
-package com.android.etcxc;
+package com.etcxc.android;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.android.etcxc.R;
 
 public class MainActivity extends AppCompatActivity {
 
