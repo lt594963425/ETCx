98957db3eac587f700cc33f54aad3874f6805eb7
xwpeng <cocpl0101001@gmail.com>
2017-06-01 15:55:48 +0800

    移动文件

---
+++ app/src/main/java/com/etcxc/android/utils/Base64Util.java
@@ -0,0 +1,81 @@
+package com.etcxc.android.utils;
+
+import android.util.Base64;
+
+import java.io.UnsupportedEncodingException;
+
+/**
+ * Base64工具类
+ * Created by xwpeng on 2015/7/15.
+ */
+public class Base64Util {
+
+    private static final String TAG = "Base64Util";
+
+    private static final char[] codec_table = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
+            'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+            'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4',
+            '5', '6', '7', '8', '9', '+', '/'};
+
+    public static String encode(String string) {
+        if (string == null || "".equals(string)) {
+            return string;
+        }
+        return encode(string.getBytes());
+    }
+
+    public static String encode(byte[] a) {
+        int totalBits = a.length * 8;
+        int nn = totalBits % 6;
+        int curPos = 0;// process bits
+        StringBuffer toReturn = new StringBuffer();
+        while (curPos < totalBits) {
+            int bytePos = curPos / 8;
+            switch (curPos % 8) {
+                case 0:
+                    toReturn.append(codec_table[(a[bytePos] & 0xfc) >> 2]);
+                    break;
+                case 2:
+
+                    toReturn.append(codec_table[(a[bytePos] & 0x3f)]);
+                    break;
+                case 4:
+                    if (bytePos == a.length - 1) {
+                        toReturn.append(codec_table[((a[bytePos] & 0x0f) << 2) & 0x3f]);
+                    } else {
+                        int pos = (((a[bytePos] & 0x0f) << 2) | ((a[bytePos + 1] & 0xc0) >> 6)) & 0x3f;
+                        toReturn.append(codec_table[pos]);
+                    }
+                    break;
+                case 6:
+                    if (bytePos == a.length - 1) {
+                        toReturn.append(codec_table[((a[bytePos] & 0x03) << 4) & 0x3f]);
+                    } else {
+                        int pos = (((a[bytePos] & 0x03) << 4) | ((a[bytePos + 1] & 0xf0) >> 4)) & 0x3f;
+                        toReturn.append(codec_table[pos]);
+                    }
+                    break;
+                default:
+                    // never hanppen
+                    break;
+            }
+            curPos += 6;
+        }
+        if (nn == 2) {
+            toReturn.append("==");
+        } else if (nn == 4) {
+            toReturn.append("=");
+        }
+        return toReturn.toString();
+
+    }
+
+    public static String decode(String str) {
+        try {
+            return new String(Base64.decode(str, Base64.DEFAULT), "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+           LogUtil.e(TAG, "decode " + str, e);
+            return null;
+        }
+    }
+}
